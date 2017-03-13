0588f62eb9a158bd1e810a3d4d59127a1af53e25
Joost Timmerman <joost.timmerman@rug.nl>
2017-03-13 17:01:02 +0100

    cleanup

--- includes/shortcodes_deprecated.php
+++ includes/shortcodes_deprecated.php
@@ -6,22 +6,7 @@
  * Since Wordpress doesn't play nice with dashes in shortcode names, we've replaced those in favor of underscores.
  * Discussion: https://github.com/theukedge/shortcodes-to-show-or-hide-content/issues/26
  */
-
-/*
-// Keep the old shortcodes for a while. Better safe than sorry.
-add_shortcode( 'time-restrict', 'time_restricted_shortcode' );
-add_shortcode( 'time-restrict-repeat', 'repeat_time_restricted_shortcode' );
-add_shortcode( 'time-restrict-repeat-1', 'repeat_time_restricted_shortcode' );
-add_shortcode( 'time-restrict-repeat-2', 'repeat_time_restricted_shortcode' );
-add_shortcode( 'time-restrict-repeat-3', 'repeat_time_restricted_shortcode' );
-*/
-
-/*
- * Helper functions to update existing shortcodes (those with dashes) to new format (with underscores).
- */
-
-function showhide_plugin_upgrade_action() {
-	// do upgrade actions
+function replace_shortcodes() {
 	global $wpdb;
 
 	// time-restrict-repeat-* to time_restrict_repeat_*
@@ -35,17 +20,26 @@ function showhide_plugin_upgrade_action() {
 	// time-restrict to time_restrict
 	$wpdb->query( "UPDATE $wpdb->posts SET post_content = replace(post_content,'[time-restrict','[time_restrict')" ); // open tag
 	$wpdb->query( "UPDATE $wpdb->posts SET post_content = replace(post_content,'[/time-restrict]','[/time_restrict]')" ); // close tag
+}
 
-	// afterwards, save new version number in options
+
+/*
+ * Helper functions for administration
+ */
+function showhide_plugin_upgrade_action() {
+	// run upgrade actions
+	replace_shortcodes(); // this function can be disabled / removed later on. The rest we can keep for future administration.
+
+	// afterwards, save new version number in WP options
 	update_option( 'showhide_plugin_version', SHOWHIDE_PLUGIN_VERSION );
 }
 
+
 function showhide_plugin_check_version() {
-	// check if current version is in the DB. If not, or non existend, run an upgrade action
+	// check if current version is in the WP options. If not, or non-existent, run upgrade action
 	if ( SHOWHIDE_PLUGIN_VERSION !== get_option( 'showhide_plugin_version' ) ) {
 		showhide_plugin_upgrade_action();
 	}
-
 }
 
 add_action( 'plugins_loaded', 'showhide_plugin_check_version' );
